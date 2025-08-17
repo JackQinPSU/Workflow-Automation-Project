9b16cb278a6b59c7cbc5ce6dc07df2480ea6a528
JackQinPSU <qxq5086@psu.edu>
2025-08-17 11:26:58 -0400

    auth react authService setup

---
+++ frontend/features/auth/authService.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const APP_AUTH_URL = process.env.APP_AUTH_URL;
+
+
+//Register user
+const register = async (userData) => {
+    const res = await axios.post(`${APP_AUTH_URL}register`, userData);
+    return res.data;
+};
+
+//Login user
+const login = async (credentials) => {
+    const res = await axios.post(`${APP_AUTH_URL}login`, credentials);
+    return res.data;
+};
+
+export { register, login };
