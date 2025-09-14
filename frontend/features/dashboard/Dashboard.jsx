a0d3d0a714a1642a95c35fc89b76370cecc981e8
JackQinPSU <qxq5086@psu.edu>
2025-09-13 23:12:04 -0400

    dashboard jsx setup complete, able to read text

--- frontend/features/dashboard/Dashboard.jsx
+++ frontend/features/dashboard/Dashboard.jsx
@@ -12,7 +12,31 @@ const Dashboard = () => {
     };
 
     //upload to backend
-    
+    const handleUpload = async () => {
+        if (!file) {
+            alert("Please select a file first!");
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append("file", file);
+
+        try {
+            setLoading(true);
+            const response = await fetch("http://localhost:5000/api/upload/", {
+                method: "POST",
+                body: formData,
+        });
+
+        const data = await response.json();
+        setResult(data.text || "No text found");
+        } catch (err) {
+            console.error("Upload failed:", err);
+            setResult("Upload failed");
+        } finally {
+            setLoading(false);
+        }
+    };
 
 
     return (
@@ -20,7 +44,7 @@ const Dashboard = () => {
             <div className="navbar">
                 <div className="logo">BookMatch</div>
                 <div className="menu">
-                    <a href="">Home</a>
+                    <a href="/dashboard">Home</a>
                     <a href="">Library</a>
                     <a href="">Profile</a>
                 </div>
@@ -28,7 +52,22 @@ const Dashboard = () => {
 
             <div className="upload-section">
                 <p className="upload-picture ">Upload a picture!</p>
-                <button className="upload-button">Upload</button>
+
+                {/* File picker */}
+                <input type="file" onChange={handleFileChange} />
+
+                {/*Upload Button*/}
+                <button className="upload-button" onClick={handleUpload} disabled={loading}>
+                    {loading ? "Uploading..." : "Upload"}
+                </button>
+
+                {/* Result display */}
+                {result && (
+                    <div className="ocr-result">
+                        <h3>Extracted Text:</h3>
+                        <p>{result}</p>
+                    </div>
+                )}
             </div>
         </div>
     );
